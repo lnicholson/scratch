b0b1dd20c4b15fc53b45d3716366dbd8f0c9adc2
lnicholson <leslie.nicholson@gmail.com>
2025-06-04 15:11:28 -0400

    Delete aap-callback.sh
    No longer needed.

--- aap-callback.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -k -H "X-Forwarded-For: $HOSTNAME" -d "host_config_key=580dde14-bbbf-4ece-a201-52979e674e51" https://jlnatlaap01.jlncorp.net/api/controller/v2/job_templates/11/callback/
